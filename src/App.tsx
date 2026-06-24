679a610bba8d8ea67f0dc3b077cef78ce7e119a3
Avinash Yadav <avinashyadav4774@gmail.com>
2026-06-24 14:33:15 +0530

    added overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-import { useEffect, useRef, useState } from "react"
-import  Otp from "./Otp"
-import { Sidebar } from "./sidebar"
-import { Navbar } from "./Navbar";
-export  function App()
+import { useState } from "react"
+import { useSearchParams } from "react-router-dom"
+
+export function App()
 {
-  const [ open, setOpen ] = useState(false);
-  return <div className="flex ">
-    <div className={`w-64 bg-red-300 fixed left-0 top-0 -translate-x-full md:translate-x-0 ${open && "translate-x-0"} h-screen`} >Sidebar</div>
-    <div className={`flex-1 bg-blue-500 md:ml-64 ${open&&"ml-64"}`}>
-      <button className="px-4 outline-2 outline-red-300 rounded-2xl text-center cursor-pointer block md:hidden " onClick={()=> setOpen(!open) }>menu</button>
-      <div>content goes here</div>
+  const [ open , setOpen ] = useState( false );
+  return <div>
+    {
+        open &&<div className=" fixed inset-0 bg-black/40 z-40" onClick={()=> setOpen( false )}></div>
+               
+    
+    }
+    <div className={` fixed -translate-x-full w-64 h-screen md:translate-x-0 ${open ? "translate-x-0": "-translate-x-full"} bg-blue-500 p-4 z-60`}>
+      <ul className="list-none">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">Home</a>
+        </li>
+      </ul>
+    </div>
+    <div className="md:ml-64 bg-green-500 h-screen z-0">
+      <button className="block md:hidden px-4 rounded-xl outline-2 outline-blue-500" onClick={()=> setOpen(!open)}>menu</button>
+      <div>Here goes the content</div>
     </div>
   </div>
-  
 }
