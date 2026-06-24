98f5efe05b1a046bb8476a3e229db378d649e441
Avinash Yadav <avinashyadav4774@gmail.com>
2026-06-24 11:23:49 +0530

    theme toggling added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { useEffect, useState } from "react"
 import  Otp from "./Otp"
+import { Sidebar } from "./sidebar"
 export  function App()
 {
-  return <div>
-      <Otp n = { 10 }></Otp>
+  const [ dark, setDark ] = useState(false);
+  useEffect(()=>{ document.documentElement.classList.toggle("dark")},[ dark ])
+  return <div className=" bg-white text-black dark:bg-black dark:text-white" onClick={()=> setDark(!dark)}>
+    <button className="px-3 py-1 rounded-xl bg-blue-600 dark:bg-blue-400">Toggle</button>
+    <Sidebar/>
       
   </div>
 }
---
+++ src/sidebar.tsx
@@ -0,0 +1,7 @@
+export function Sidebar()
+{
+    return <div className="flex h-screen">
+        <div className="hidden  w-64 sm:block border-r-2 text-center dark:bg-gray-900">it is having something</div>
+        <div className="flex-1 text-center">no thing in it having to say that tit is going to take the widtha as long as </div>
+    </div>
+}
